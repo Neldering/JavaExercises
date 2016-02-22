36dbc9377c382d494a2c5d4aea41963ce84e4a7b
Nicolas Eldering <eldering.nicolas@gmail.com>
2016-02-22 13:41:36 -0500

    Update VendingMachine.java

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,67 +1,68 @@
 import java.util.Scanner;
 
-class Candy {
-	String Brand;
-	String Letter;
-	int Number;
-	double Price;
+class candy {
+	String brand;
+	String letter;
+	int number;
+	double price;
 }
 public class VendingMachine {
 	public static void main ( String[] args ) {
 	Candy MilkyWay, Hersheys, ReesesCup, Take5;
-	String ChoiceChar;
-	int ChoiceNum;
-	double Cash = 0.00, Change = 0.00;
+	String choiceChar;
+	int choiceNum;
+	double cash = 0.00, change = 0.00;
 	
 	Scanner kb = new Scanner(System.in);
 	
-	MilkyWay = new Candy();
-	MilkyWay.Brand = "MilkyWay";
-	MilkyWay.Letter = "A";
-	MilkyWay.Number = 1;
-	MilkyWay.Price = 1.00;
+	MilkyWay = new candy();
+	MilkyWay.brand = "MilkyWay";
+	MilkyWay.letter = "A";
+	MilkyWay.number = 1;
+	MilkyWay.price = 1.00;
 	
-	Hersheys = new Candy();
-	Hersheys.Brand = "Hersheys";
-	Hersheys.Letter = "M";
-	Hersheys.Number = 4;
-	Hersheys.Price = .75;
+	Hersheys = new candy();
+	Hersheys.brand = "Hersheys";
+	Hersheys.letter = "M";
+	Hersheys.number = 4;
+	Hersheys.price = .75;
 	
-	ReesesCup = new Candy();
-	ReesesCup.Brand = "ReesesCup";
-	ReesesCup.Letter = "R";
-	ReesesCup.Number = 8;
-	ReesesCup.Price = 1.25;
+	ReesesCup = new candy();
+	ReesesCup.brand = "ReesesCup";
+	ReesesCup.letter = "R";
+	ReesesCup.number = 8;
+	ReesesCup.price = 1.25;
 	
-	Take5 = new Candy();
-	Take5.Brand = "TakeFive";
-	Take5.Letter = "B";
-	Take5.Number = 9;
-	Take5.Price = 1.25;
+	Take5 = new candy();
+	Take5.brand = "TakeFive";
+	Take5.letter = "B";
+	Take5.number = 9;
+	Take5.price = 1.25;
    
 	Candy[] list = { MilkyWay, Hersheys, ReesesCup, Take5 };
    
-	for ( Candy c: list ) {
-		System.out.println("Brand: " + c.Brand + "\nLocation: " + c.Letter + c.Number +"\n" + "Cost" + c.Price + "\n");
+	for ( candy c: list ) {
+		System.out.println("Brand: " + c.brand + "\nLocation: " + c.letter + c.number +"\n" + "Cost" + c.price + "\n");
 	}
 	
    System.out.println("This Vending Machine dispences the Candy listed above:");
 	System.out.print("Please Enter your Money: ");
-	Cash = kb.nextDouble();
+	cash = kb.nextDouble();
 	
 	System.out.print("Please Enter the letter of your selection ");
-	ChoiceChar = kb.next();
+	choiceChar = kb.next();
 	
 	System.out.print("Please Enter the number of your selection ");
-	ChoiceNum = kb.nextInt();
+	choiceNum = kb.nextInt();
 	System.out.println();
 	
-	for ( Candy c : list )
-		if ( c.Letter.equals(ChoiceChar) && c.Number == ChoiceNum ) {
-			if ( c.Price <= Cash ) {
-				Change = Cash - c.Price;
-				System.out.println("Here is your " + c.Brand );
+	for ( candy c : list )
+		if ( c.letter.equalsIgnoreCase(choiceChar) && c.number == choiceNum ) {
+			if ( c.price <= cash ) {
+				change = cash - c.price;
+				System.out.println("Here is your " + c.brand );
 				System.out.println("Your change is : " + Change);
+				
 			}
 			else
 				System.out.println("Insufficient Funds");
